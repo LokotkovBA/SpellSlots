d27f415b484e962943a1c0c1319c1589b86604fd
LokotkovBA <woweuro2010@gmail.com>
2025-02-22 14:36:01 +0300

    fix: text inside buttons selecting randomly

--- src/components/MainButton.vue
+++ src/components/MainButton.vue
@@ -1,6 +1,6 @@
 <template>
     <button
-        class="rounded bg-yellow-600 px-2 py-1 text-sm text-slate-950 disabled:bg-gray-600"
+        class="rounded bg-yellow-600 px-2 py-1 text-sm text-slate-950 select-none disabled:bg-gray-600"
     >
         <slot />
     </button>
